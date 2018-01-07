87c2251e5057f71a731558be8e9e07b638a51da0
Himavarsha Kagidala <himavarshakagidala@Himavarshas-MacBook-Pro.local>
2018-01-06 22:36:55 -0800

    dfs

---
+++ src/main/java/com/lab/graph/dfs/Vertex.java
@@ -0,0 +1,15 @@
+package com.lab.graph.dfs;
+
+public class Vertex {
+
+	String name;
+	int state;
+	
+	public Vertex(String name) {
+		this.name = name;
+	}
+	
+	public String toString(){
+		return name;
+	}
+}
